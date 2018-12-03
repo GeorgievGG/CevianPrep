1ab58ed615bf478f01d982ab7ccd37cd0eda0cbb
GeorgievGG <Juvetikon@abv.bg>
2018-12-03 23:31:58 +0200

    Revert app.ts to the correct version.

--- BlocG/app.ts
+++ BlocG/app.ts
@@ -1,20 +1,15 @@
 import express = require('express');
 import ejs = require('ejs');
-import bodyParser = require('body-parser');
-import { HomeController, RegistrationController } from './Controllers';
+import { HomeController } from './Controllers';
 
 const app: express.Application = express();
 const port: number = 5068;
 const renderEjsFile: Function = ejs.renderFile;
-const urlEncodedParser = bodyParser.urlencoded({ extended: false });
-const jsonParser = bodyParser.json();
 
 app.engine('html', renderEjsFile);
 app.set('view engine', 'html');
-app.use(urlEncodedParser);
-app.use(jsonParser);
+
 app.use('/', HomeController);
-app.use('/api/Register', RegistrationController);
 
 app.listen(port, () => {
     console.log('Server launched!');
