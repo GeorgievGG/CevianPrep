a113159ed4e1ac174f8bdf92cf60ca23dc627256
GeorgievGG <ggfinz@gmail.com>
2019-01-07 12:25:50 +0200

    Refactor JWT authentication to fit with the current logic.

--- BlocG/src/services/passport.ts
+++ BlocG/src/services/passport.ts
@@ -1,24 +1,24 @@
 import passport from 'passport';
-import passportLocal from 'passport-local';
 import passportJWT from 'passport-jwt';
-import { User } from '../models/user';
+import passportLocal from 'passport-local';
 import { IUserModel } from '../interfaces/IUserModel';
+import { User } from '../models/user';
 
 const LocalStrategy = passportLocal.Strategy;
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(new LocalStrategy({
-        usernameField: 'username',
-        passwordField: 'password'
-    }, 
+    usernameField: 'username',
+    passwordField: 'password'
+},
     function (username: string, password: string, cb: any) {
-        return User.findOne({username: username, password: password})
+        return User.findOne({ username: username, password: password })
             .then(user => {
                 if (!user) {
-                    return cb(null, false, {message: 'Incorrect email or password.'});
+                    return cb(null, false, { message: 'Incorrect email or password.' });
                 }
-                return cb(null, user, {message: 'Logged In Successfully'});
+                return cb(null, user, { message: 'Logged In Successfully' });
             })
             .catch(err => cb(err));
     }
@@ -26,16 +26,13 @@ passport.use(new LocalStrategy({
 
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey   : 'KzLKzLKzLKzL'
+    secretOrKey: 'KzLKzLKzLKzL'
 },
-function (jwtPayload: IUserModel, cb: any) {
-    //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
-    return User.findById(jwtPayload._id)
-        .then(user => {
-            return cb(null, user);
-        })
-        .catch(err => {
-            return cb(err);
-        });
-}
+    function (jwtPayload: IUserModel, cb: any) {
+        if (!jwtPayload) {
+            return cb(new Error("No payload found."));
+        }
+
+        return cb(null, jwtPayload);
+    }
 ));
