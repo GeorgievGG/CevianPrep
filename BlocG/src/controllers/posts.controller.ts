620e71aa0dc8eaf5e8af774b6b6aab35a386ae98
GeorgievGG <ggfinz@gmail.com>
2019-01-07 15:06:26 +0200

    Add API for all posts retrieval. Fix bug for posts' user id assignment.

--- BlocG/src/controllers/posts.controller.ts
+++ BlocG/src/controllers/posts.controller.ts
@@ -1,16 +1,24 @@
 import { Request, Response, Router } from 'express';
-import { Model } from 'mongoose';
 import passport from 'passport';
 import { IPost } from '../interfaces/IPost';
 import { IPostModel } from '../interfaces/IPostModel';
 import { IUserModel } from '../interfaces/IUserModel';
 import { ContentResponse } from '../models/contentResponse';
 import { Post } from '../models/post';
+import { User } from '../models/user';
 
 const router: Router = Router();
 const internalServerErrorMessage = 'Internal server error';
 const alreadyExistsErrorMessage = 'Post already exists';
 const unauthorizedErrorMessage = 'Unauthorized!';
+
+router.get('/posts', (req: Request, res: Response) => {
+  authenticateUser(req, res)
+    .then(() => getAllUsers())
+    .then((users: IUserModel[]) => getAllPosts(users))
+    .then((posts: IPost[]) => sendJsonResponse(posts, res))
+    .catch((contentResponse: ContentResponse) => sendResponse(contentResponse, res));
+});
 
 router.post('/users/posts', (req: Request, res: Response) => {
   authenticateUser(req, res)
@@ -34,13 +42,51 @@ function authenticateUser(req: Request, res: Response) {
   })
 }
 
+function getAllUsers() {
+  return new Promise<IUserModel[]>(function (resolve, reject) {
+    User.find({}, function (err: Error, users: IUserModel[]) {
+      if (err) {
+        console.log(err);
+        reject(new ContentResponse(500, internalServerErrorMessage));
+      }
+
+      resolve(users);
+    });
+  })
+}
+
+function getAllPosts(users: IUserModel[]) {
+  return new Promise<IPost[]>(function (resolve, reject) {
+    Post.find({}, function (err: Error, posts: IPostModel[]) {
+      if (err) {
+        console.log(err);
+        reject(new ContentResponse(500, internalServerErrorMessage));
+      }
+      var simplePosts: IPost[] = posts.map(post => <IPost>{
+        created: post.created,
+        content: post.content,
+        title: post.title,
+        user: (<IUserModel>users.find(byId(post))).username
+      });
+
+      resolve(simplePosts);
+    });
+  })
+
+  function byId(post: IPostModel): (value: IUserModel, index: number, obj: IUserModel[]) => boolean {
+    return function (element: IUserModel) {
+      return element._id == post.user;
+    };
+  }
+}
+
 function createPost(user: IUserModel, req: Request) {
   return new Promise<IPost>(function (resolve) {
     let post: IPost = {
       created: req.body.created,
       content: req.body.content,
       title: req.body.title,
-      user: user.id
+      user: user._id
     };
 
     resolve(post);
@@ -49,7 +95,7 @@ function createPost(user: IUserModel, req: Request) {
 
 function validatePost(post: IPost) {
   return new Promise<IPost>(function (resolve, reject) {
-    Post.find({ created: post.created, user: post.user }, function (err: Error, posts: Model<IPostModel>[]) {
+    Post.find({ created: post.created, user: post.user }, function (err: Error, posts: IPostModel[]) {
       if (err) {
         console.log(err);
         reject(new ContentResponse(500, internalServerErrorMessage));
@@ -75,6 +121,12 @@ function savePost(post: IPost) {
         resolve(new ContentResponse(200, `Post ${post.content} successfully added!`));
       }
     });
+  })
+}
+
+function sendJsonResponse(objectArray: Object[], res: Response) {
+  return new Promise<void>(function () {
+    res.json(objectArray);
   })
 }
 
