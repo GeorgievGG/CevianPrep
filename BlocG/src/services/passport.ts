a7bd2da1da1278e111f14a24f390e1001575dc7e
GeorgievGG <ggfinz@gmail.com>
2019-01-07 12:23:04 +0200

    Include JWT logic in the passport service for protected requests.

--- BlocG/src/services/passport.ts
+++ BlocG/src/services/passport.ts
@@ -1,8 +1,12 @@
 import passport from 'passport';
 import passportLocal from 'passport-local';
+import passportJWT from 'passport-jwt';
 import { User } from '../models/user';
+import { IUserModel } from '../interfaces/IUserModel';
 
 const LocalStrategy = passportLocal.Strategy;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(new LocalStrategy({
         usernameField: 'username',
@@ -18,4 +22,20 @@ passport.use(new LocalStrategy({
             })
             .catch(err => cb(err));
     }
+));
+
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey   : 'KzLKzLKzLKzL'
+},
+function (jwtPayload: IUserModel, cb: any) {
+    //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
+    return User.findById(jwtPayload._id)
+        .then(user => {
+            return cb(null, user);
+        })
+        .catch(err => {
+            return cb(err);
+        });
+}
 ));
